66c35a1762567b584e3b0b1bdb518f9ef499c9ba
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-11-05 11:59:34 -0500

    reducer file done

--- my-app/src/reducers/reducer.js
+++ my-app/src/reducers/reducer.js
@@ -4,7 +4,53 @@ export const initialState = [
 {
     item: 'Learn about reducers',
     completed: false,
-    id: 3892987589
+    id: 1
   },
 
 ]
+
+export const todoReducer = (state, action) => {
+    switch (action.type) {
+
+// ADD TODO
+ case "ADDTODO":
+     return [...state,
+    {
+        item: action.payload,
+        completed: false,
+        id: Date.now()
+    }];
+
+// Toggle Todo
+
+case "TOGGLE":
+    let Clicktoggle =
+    state.map(item => {
+        if(item.id === action.payload) {
+     return {
+        ...item,
+        completed: !item.completed
+     }
+    } else {
+        return item;
+    }
+    });
+    return Clicktoggle;
+
+// Clear Todo
+case 'CLEARTODO':
+    let Clear =
+    state.filter(item => {
+        if (item.complteed === true){
+            return !item.completed   
+        }else{
+            return item;
+        }
+    })
+    return Clear;
+
+
+default: 
+return state;
+ };
+}
